4a3ac0201c6eec674525be7c3baba5af2d963dc7
mito <78819172+mitoiscool@users.noreply.github.com>
2024-07-19 11:04:48 -0500

    Install records

--- boron/routes/application.py
+++ boron/routes/application.py
@@ -8,16 +8,20 @@ from flask import (
     make_response
 )
 from boron.util.authenticator import logged_in
-from boron.routes.auth import auth
+from boron.util.db import query
 
 application = Blueprint("application", __name__, url_prefix="/applications/")
 
 @application.get("home")
 def applications_home():
 
-    if not logged_in(request):
-        return make_response(redirect(url_for(auth.login))) # NOTE THIS DOES NOT WORK DO TO CIRCULAR IMPORTS
+    #if not logged_in(request):
+        #return make_response(redirect(url_for(auth.login))) # NOTE THIS DOES NOT WORK DO TO CIRCULAR IMPORTS
     
+    query("SELECT ")
+    
+    
+    return render_template("panel/panel_home.html", ctx=)
 
     return ""
 
@@ -25,46 +29,46 @@ def applications_home():
 @application.get("create")
 def get_create_app():
 
-    if not logged_in(request):
-        return make_response(redirect(url_for(auth.login)))
+    #if not logged_in(request):
+        #return make_response(redirect(url_for(auth.login)))
     return ""
 
 # Create app (post)
 @application.post("create")
 def create_app():
 
-    if not logged_in(request):
-        return make_response(redirect(url_for(auth.login)))
+    #if not logged_in(request):
+        #return make_response(redirect(url_for(auth.login)))
     return ""
 
 # Delete app (post)
 @application.post("/delete")
 def delete_app():
 
-    if not logged_in(request):
-        return make_response(redirect(url_for(auth.login)))
+    #if not logged_in(request):
+        #return make_response(redirect(url_for(auth.login)))
     return ""
 
 # App home page
 @application.get("<appid>/")
 def get_app(appid):
 
-    if not logged_in(request):
-        return make_response(redirect(url_for(auth.login)))
+    #if not logged_in(request):
+        #return make_response(redirect(url_for(auth.login)))
     return ""
 
 # App keys page
 @application.get("<appid>/keys/")
 def get_app_keys(appid):
 
-    if not logged_in(request):
-        return make_response(redirect(url_for(auth.login)))
+    #if not logged_in(request):
+        #return make_response(redirect(url_for(auth.login)))
     return ""
 
 # App users page
 @application.get("<appid>/users/")
 def get_app_users(appid):
 
-    if not logged_in(request):
-        return make_response(redirect(url_for(auth.login)))
+    #if not logged_in(request):
+        #return make_response(redirect(url_for(auth.login)))
     return ""
