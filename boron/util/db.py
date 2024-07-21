ce9ced72674f9561317381ba532943d8ab1df1cc
misaka10987 <misaka10987@outlook.com>
2024-07-21 10:22:19 +0800

    seperate lazy and eager queries

--- boron/util/db.py
+++ boron/util/db.py
@@ -1,8 +1,17 @@
 from flask import current_app
-import records
 from loguru import logger
 
 
 def query(query: str, args: dict = {}):
+    """
+    Execute query over database, cache all results and return.
+    """
+    return [row for row in query_it(query, args)]
+
+
+def query_it(query: str, args: dict = {}):
+    """
+    Execute query over database, return an iterator.
+    """
     logger.trace(f"database: query = '{query}', args = {args}")
-    return current_app.db.query(query, fetchall=True, **args)
+    return current_app.db.query(query, **args)
