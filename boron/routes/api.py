2f273fa46b0b0b90ced41b9f771825d3897dfb70
misaka10987 <misaka10987@outlook.com>
2024-07-23 06:05:15 +0800

    fix multiple definition error

--- boron/routes/api.py
+++ boron/routes/api.py
@@ -1,24 +1,17 @@
 from flask import (
     Blueprint,
-    abort,
-    render_template,
     request,
-    redirect,
-    url_for,
 )
-from boron.util.general import gen_license
-from boron.util.db import query
 from boron.util.appuser import create, login, logout
-from datetime import datetime, timedelta
 
 
 api = Blueprint("api", __name__, url_prefix="/api/")
 
+
 @api.post("client/register")
 def register_user():
-
     """Use license key, initialize expiration date and create use
-    
+
     args:
     username
     password
@@ -26,11 +19,11 @@ def register_user():
     license
     """
 
-    formUsername = request.form.get('user')
-    formPassword = request.form.get('pass')
+    formUsername = request.form.get("user")
+    formPassword = request.form.get("pass")
 
-    formLicense = request.form.get('licensekey')
-    formAppId = request.form.get('app_id')
+    formLicense = request.form.get("licensekey")
+    formAppId = request.form.get("app_id")
 
     # get license key info
 
@@ -40,7 +33,7 @@ def register_user():
 @api.post("client/login")
 def login_user():
     """Login, return session
-    
+
     args:
     username
     password
@@ -48,33 +41,35 @@ def login_user():
 
     """
 
-    formUsername = request.form.get('user')
-    formPassword = request.form.get('pass')
+    formUsername = request.form.get("user")
+    formPassword = request.form.get("pass")
 
-    formAppId = request.form.get('app_id')
+    formAppId = request.form.get("app_id")
 
     return login(formUsername, formPassword, formAppId)
 
+
 @api.post("client/logout")
-def login_user():
+def logout_user():
     """Logout
-    
+
     args:
     session
     appid
 
     """
 
-    formSess = request.form.get('session')
+    formSess = request.form.get("session")
 
-    formAppId = request.form.get('app_id')
+    formAppId = request.form.get("app_id")
 
     return logout(formSess, formAppId)
+
 
 @api.post("client/redeem")
 def redeem_user():
     """Use license key, initialize expiration date and create use
-    
+
     args:
 
     session
@@ -82,4 +77,3 @@ def redeem_user():
     licensekey
 
     """
-
