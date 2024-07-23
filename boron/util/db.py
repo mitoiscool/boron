4db0805c82c0d1908231b338fda5400f0b00eecc
misaka10987 <misaka10987@outlook.com>
2024-07-24 04:55:24 +0800

    add type notation

--- boron/util/db.py
+++ boron/util/db.py
@@ -1,22 +1,25 @@
 from flask import current_app
 from loguru import logger
+from typing import Iterable, List
+from records import Record
 
 
-def query(query: str, args: dict = {}):
+def query(query: str, args: dict = {}) -> List[Record]:
     """
     Execute query over database, cache all results and return.
     """
     return [row for row in query_it(query, args)]
 
 
-def query_it(query: str, args: dict = {}):
+def query_it(query: str, args: dict = {}) -> Iterable[Record]:
     """
     Execute query over database, return an iterator.
     """
     logger.trace(f"database: query = '{query}', args = {args}")
     return current_app.db.query(query, **args)
 
-def record_exists(quer: str, args: dict = {}):
+
+def record_exists(quer: str, args: dict = {}) -> bool:
     q = query(quer, args)
 
-    return q != None and len(q) > 0
+    return q is not None and len(q) > 0
